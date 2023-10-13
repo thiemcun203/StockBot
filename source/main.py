434e0589ce41037f9d2ef203b6422b4ff55f105d
Nguyễn Bá Thiêm <104337976+thiemcun203@users.noreply.github.com>
2023-10-13 22:22:27 +0700

    mmm

--- source/main.py
+++ source/main.py
@@ -22,7 +22,7 @@ and if the answer is not contained within the text below, say 'Tôi không biế
 human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key='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')
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key='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')
 conversation = ConversationChain(memory=st.session_state.buffer_memory,prompt=prompt_template, llm=llm, verbose=True)
 
 
